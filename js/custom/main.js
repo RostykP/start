6b9a33f7ffe12303666271a58f3b652efe5ccb41
yaroslaw_gutnyk <slavic@mass.im>
2016-06-25 13:45:49 +0300

    mark-up v.1

---
+++ js/custom/main.js
@@ -0,0 +1,18 @@
+/*jslint unparam: true, browser: true, indent: 2 */
+(function($, window, document, undefined) {
+    'use strict';
+
+    var $doc = $(document),
+        $window = $(window);
+
+
+    $doc.ready(function(jQuery) {
+
+        
+        $('input[name="daterange"]').daterangepicker({
+            
+        });
+    });
+
+
+}(jQuery, window, document));
