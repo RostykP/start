5254d1e9a6ebef600d2b7331cea0828b9ad660f4
yaroslaw_gutnyk <slavic@mass.im>
2016-06-26 20:08:50 +0300

    tweak

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,17 +1,59 @@
 /*jslint unparam: true, browser: true, indent: 2 */
-(function($, window, document, undefined) {
+(function ($, window, document, undefined) {
     'use strict';
 
     var $doc = $(document),
         $window = $(window);
 
 
-    $doc.ready(function(jQuery) {
+    $doc.ready(function (jQuery) {
 
-        
-        $('input[name="daterange"]').daterangepicker({
-            
+        $('#submit').click(function (e) {
+            e.preventDefault();
+            var login = $('input[name="login"]').val(),
+                pass = $('input[name="pass"]').val();
+
+            var sid = getResp({"login": login, "pass": pass}, 'auth/');
+            if(sid.result){
+                // $.removeCookie('sid')
+                $.cookie('sid', sid.sid);
+                window.location.href = $(location).attr('href').slice(0, -10) + 'index.html';
+            }else{
+                alert(sid.msg);
+            }
+
+
+
         });
+        $('input[name="daterange"]').daterangepicker({});
+
+
+
+
+
+        function getResp(data, url) {
+            var myVariable;
+            $.ajax({
+                type: "POST",
+                crossDomain: true,
+                'global': false,
+                'async': false,
+                dataType: "json",
+                data: JSON.stringify(data),
+                url: "http://95.46.98.99/sys/" + url,
+                success: function (data) {
+                    myVariable = data;
+
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(xhr.status);
+                    console.log(thrownError);
+                }
+            });
+            return myVariable;
+        }
+
+
     });
 
 
